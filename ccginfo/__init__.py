af78a498311d4f124b8af4062214fdbcc3aeea2a
Martastain <martas@imm.cz>
2019-02-08 23:25:52 +0100

    fixed 2.0.6/tvp version reporting

--- ccginfo/__init__.py
+++ ccginfo/__init__.py
@@ -115,6 +115,11 @@ class Caspar207InfoParser(BaseInfoParser):
 
 
 
+class Caspar206InfoParser(Caspar207InfoParser):
+    protocol = 2.06
+
+
+
 
 
 class Caspar21InfoParser(BaseInfoParser):
@@ -260,7 +265,7 @@ def get_info_parser(caspar):
             "2.2" : Caspar22InfoParser,
             "2.1"   : Caspar21InfoParser,
             "2.0.7" : Caspar207InfoParser,
-            "2.0.6" : Caspar207InfoParser, # Same xml as 2.0.7
+            "2.0.6" : Caspar206InfoParser, # Same xml as 2.0.7
         }
 
     Parser = None
